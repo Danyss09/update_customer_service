88acc8db706445a18be812566cfdaee6e612fb49
Danyss09 <112229585+Danyss09@users.noreply.github.com>
2025-02-06 18:52:46 -0500

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from controllers.customer_controller import customer_controller
-app = Flask(__name__)
-
-# Registrar el Blueprint
-app.register_blueprint(customer_controller)
-
-if __name__ == '__main__':
-    app.run(debug=True)
