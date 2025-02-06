ead944bfd345a02a2aea01be57d4a1ebed886556
Danyss09 <danielacaceresp4@gmail.com>
2025-02-05 19:26:46 -0500

    update files

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ app = Flask(__name__)
 # Registrar el Blueprint
 app.register_blueprint(customer_controller)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
